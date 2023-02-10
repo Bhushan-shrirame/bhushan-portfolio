4ca11f8d27a7932ea52a47d9423a79a791f90c07
Bhushan-shrirame <bhushanshrirame24@gmail.com>
2023-02-10 22:48:17 +0530

    get in touch working

--- src/components/container/Contact/Contact.jsx
+++ src/components/container/Contact/Contact.jsx
@@ -5,86 +5,6 @@ import { socialIcons } from '../../../Data';
 import { motion } from 'framer-motion';
 import { useForm, ValidationError } from '@formspree/react';
 
-// const Contact = () => {
-
-//   const [state, handleSubmit] = useForm("mbjegkqk");
-//   if (state.succeeded) {
-//       return <p>Thanks for joining!</p>;
-//   }
-//   return (
-//     <div className="container" id="contact">
-//       <motion.div
-//         initial={{opacity: 0}}
-//         whileInView={{y: [-50, 0], opacity: 1}} 
-//         className="title"
-//       >
-//             <span>get in touch</span>
-//             <h1>Contact Me</h1>
-//       </motion.div>
-//       <div className="contact_form">
-//         <motion.div
-//           initial={{x: 0, opacity: 0}}
-//           whileInView={{ x: [-150,0], opacity: 1 }}
-//           transition={{duration: 1}}
-//           className='contact_left_container'>
-//           <h3>Just Say Hi</h3>
-//           <p className='contact_text'>I am so excited to work with you! I am looking forward to learning new skills, collaborating on projects, and growing together. I am confident that our partnership will be successful and rewarding. I am eager to get started and make a positive impact. I believe that our combined efforts will lead to great success. I can't wait to see what we can accomplish together!</p>
-//           {contacts.map(contact => {
-//             return (
-//               <div className='contact_left' key={contact.id}>
-//                 <div className="icon">
-//                   {contact.icon}
-//                 </div>
-//                 <p>{ contact.infoText}</p>
-//               </div>
-//             )
-//           })}
-//           <div className="social_icons" >
-//                   {socialIcons.map((socialIcon, index) => {
-//                   return (
-//                       <div key={index} onClick={() => window.location.href = socialIcon.link}>
-//                           {socialIcon.icon}
-//                       </div>        
-//                   )
-//               })}
-//               </div>
-//         </motion.div>
-
-//          {/* this  */}
-
-//         {/* <motion.div
-//           initial={{x: 0, opacity: 0}}
-//           whileInView={{ x: [150,0], opacity: 1 }}
-//           transition={{duration: 1}}
-//           className="contact_right"
-//          >
-//           <h3>Get In Touch</h3>
-//           <div className="row">
-//             <input type="text" placeholder='Name' name='username' />
-     
-//           </div>
-//           <div className="row">
-            
-//             <input type="email" placeholder='Email' name='Email' />
-//           </div>
-//           <div className="row">
-//             <textarea placeholder='message' name='message'></textarea>
-//           </div>
-//           <motion.div
-//             whileHover={{ scale: 1.1 }}
-//             transition={{duration: 0.3}}
-//             className="btn"
-//           >
-//             <a href="https://formspree.io/f/mbjegkqk">Send</a>
-//           </motion.div>
-//         </motion.div> */}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Contact
-
 function Contact() {
   const [state, handleSubmit] = useForm("mbjegkqk");
   if (state.succeeded) {
@@ -161,7 +81,7 @@ function Contact() {
         prefix="Email" 
         field="email"
         className="row"
-        // errors={state.errors}
+        errors={state.errors}
       />
       </div> 
    
@@ -176,7 +96,7 @@ function Contact() {
       <ValidationError 
         prefix="Message" 
         field="message"
-        // errors={state.errors}
+        errors={state.errors}
       />
       
       <motion.div
